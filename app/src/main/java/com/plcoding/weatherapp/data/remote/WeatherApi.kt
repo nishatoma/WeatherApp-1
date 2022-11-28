54e8ef396b91a5a85a4ad7a7e0ae2bd1f4ec15df
Nisha Shlaymoon <nishatoma@gmail.com>
2022-11-27 19:06:38 -0500

    push

---
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,14 @@
+package com.plcoding.weatherapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    // Call Api endpoint
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    suspend fun getWeatherData(
+        @Query("latitude") lat: Double,
+        @Query("longtitude") long: Double
+    ): WeatherDto
+}
